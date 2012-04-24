2dac0e8e6c3dc9361e458e04f624dce3ce7112ce
Peter Labaj <peter.labaj@gmail.com>
2012-04-24 05:04:34 +0200

    Fix named route use issue with Rails 3.2

--- app/controllers/spree/static_content_controller.rb
+++ app/controllers/spree/static_content_controller.rb
@@ -15,7 +15,7 @@ class Spree::StaticContentController < Spree::BaseController
       request.path
     end
     path = path.gsub('//','/')
-    path = StaticPage::remove_spree_mount_point(path) unless Rails.application.routes.named_routes[:spree].path == "/"
+    path = StaticPage::remove_spree_mount_point(path) unless Rails.application.routes.named_routes[:spree].path.spec.to_s == "/"
     
     unless @page = Spree::Page.visible.find_by_slug(path)
       render_404
